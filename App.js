a39d0794df86dda3bf25efa2887edc44f33e1441
Bhargodev Arya <bhargodevarya@gmail.com>
2020-07-16 19:44:17 +0530

    Adding goal initial commit

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, Button} from 'react-native';
 
 export default function App() {
+  const [enteredText, setEnteredText] = useState('')
+  const [goalList, setGoals] = useState([])
+
+  const handleTextUpdate = (text) => {
+    setEnteredText(text)
+  }
+
+  const addGoal = () => {
+    setGoals(goals => [...goals, enteredText])
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={{paddingTop: '30px'}}>
+        <TextInput placeHolder="Course Goal" style={{borderColor:'black'}} 
+        onChangeText={handleTextUpdate} value={enteredText}></TextInput>
+        <Button title="Add" onPress={addGoal}></Button>
+      </View>
+      <View>
+        {goalList.map(goal => <Text key="goal">{goal}</Text>)}
+      </View>
     </View>
   );
 }
