950b30c3c88fdd9d98dc3d53c7d7c8b2a0d2f121
Bhargodev Arya <bhargodevarya@gmail.com>
2020-07-16 21:00:28 +0530

    Changed ScrollView to FlatList

--- App.js
+++ App.js
@@ -1,29 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import React, {useState} from 'react';
-import { StyleSheet, Text, View, TextInput, Button, ScrollView} from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  ScrollView,
+  FlatList,
+} from "react-native";
+/**
+ * ScrollView does eager render
+ */
 export default function App() {
-  const [enteredText, setEnteredText] = useState('')
-  const [goalList, setGoals] = useState([])
+  const [enteredText, setEnteredText] = useState("");
+  const [goalList, setGoals] = useState([]);
 
   const handleTextUpdate = (text) => {
-    setEnteredText(text)
-  }
+    setEnteredText(text);
+  };
 
   const addGoal = () => {
-    setGoals(goals => [...goals, enteredText])
-  }
+    setGoals((goals) => [...goals, enteredText]);
+  };
 
   return (
     <View style={styles.container}>
-      <View style={{paddingTop: '30px'}}>
-        <TextInput placeHolder="Course Goal" style={{borderColor:'black'}} 
-        onChangeText={handleTextUpdate} value={enteredText}></TextInput>
+      <View style={{ paddingTop: "30px" }}>
+        <TextInput
+          placeHolder="Course Goal"
+          style={{ borderColor: "black" }}
+          onChangeText={handleTextUpdate}
+          value={enteredText}
+        ></TextInput>
         <Button title="Add" onPress={addGoal}></Button>
       </View>
-      <ScrollView>
-        {goalList.map(goal => <View key="goal" style={styles.listItem}><Text>{goal}</Text></View>)}
-      </ScrollView>
+      <FlatList
+        data={goalList}
+        renderItem={itemList => (
+          <View style={styles.listItem}>
+            <Text>{itemList.item}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
@@ -31,16 +50,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 
   listItem: {
     padding: 10,
-    borderColor: 'black',
+    borderColor: "black",
     borderWidth: 1,
-    backgroundColor: 'grey',
-    marginVertical: 10
-  }
+    backgroundColor: "grey",
+    marginVertical: 10,
+  },
 });
